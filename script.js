bf0e9ef201ec0d508a264164ccdbc41e90da5aa0
maria-canals <mariacanals23@gmail.com>
2021-01-20 12:56:04 +0100

    solved

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-$(function () {
-  $("form").submit(function (event) {
-    event.preventDefault();
+const App = {
+  data() {
+    return {
+      height: "",
+      weight: "",
+      result: "",
+    };
+  },
+  methods: {
+    getFormula(e) {
+      this.result = (
+        this.weight /
+        ((this.height * this.height) / 10000)
+      ).toFixed(2);
+    },
+  },
+};
 
-    let height = $("#height").val();
-    let weight = $("#weight").val();
-
-    let img = (weight / ((height * height) / 10000)).toFixed(2);
-
-    $("#results").text(img);
-
-    if (img < 18.6) {
-      $("p:nth-of-type(1)").css("color", "red");
-    } else if (img >= 18.6 && 24.9 >= img) {
-      $("p:nth-of-type(2)").css("color", "green");
-    } else {
-      $("p:nth-of-type(3)").css("color", "red");
-    }
-  });
-});
+Vue.createApp(App).mount(".container");
